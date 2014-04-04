1cf0bc8c9176de53e557a639159c44d2079278ed
kanarinka <kanarinka@gmail.com>
2014-04-04 17:03:58 -0400

    more fixes for install process

--- www/text_processing/geoprocessing.py
+++ www/text_processing/geoprocessing.py
@@ -92,6 +92,9 @@ def lookupCountryCapitalCity(geodata):
 # be careful not to modify geodata object while iterating it bc it 
 # produces weird behavior
 def lookupContinentAndRegion(geodata):
+	if not "primaryCountries" in geodata:
+		return geodata
+		
 	primaryRegions = []
 	primaryContinents = []
 
