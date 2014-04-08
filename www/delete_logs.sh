114d357922c19096a1a688e4c3a168785470590c
kanarinka <kanarinka@gmail.com>
2014-04-08 06:20:32 -0400

    log clean up script

---
+++ www/delete_logs.sh
@@ -0,0 +1,5 @@
+rm /var/www/terra-incognita.co/Terra-Incognita/www/celery1.log
+rm /var/www/terra-incognita.co/Terra-Incognita/www/celery2.log 
+rm /var/www/terra-incognita.co/Terra-Incognita/www/server.log
+rm /var/log/terra-access.log
+rm /var/log/terra-error.log
