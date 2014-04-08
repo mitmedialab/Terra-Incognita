a126d7611758321c4852a73cc2752c030d55a644
kanarinka <kanarinka@gmail.com>
2014-04-08 06:22:55 -0400

    bugs

--- www/delete_logs.sh
+++ www/delete_logs.sh
@@ -1,5 +1,5 @@
 rm /var/www/terra-incognita.co/Terra-Incognita/www/celery1.log
 rm /var/www/terra-incognita.co/Terra-Incognita/www/celery2.log 
-rm /var/www/terra-incognita.co/Terra-Incognita/www/server.log
+rm /var/www/server.log
 rm /var/log/terra-access.log
 rm /var/log/terra-error.log
