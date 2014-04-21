e87682cccea06fc3aa614f594eccf0c0d0990637
kanarinka <kanarinka@gmail.com>
2014-04-20 20:44:30 -0400

    Endless data cleaning of cities

--- www/test/lookup_cities_from_geonames.py
+++ www/test/lookup_cities_from_geonames.py
@@ -6,11 +6,10 @@ import requests
 import requests.exceptions
 import json
 
-CITIES = list(csv.DictReader(open("1000Cities.csv",'rU')))
-COUNTRIES = list(csv.DictReader(open("countries_temp.csv",'rU')))
+CITIES = list(csv.DictReader(open("../static/data/1000cities.csv",'rU')))
 
 test_file = open('1000Cities_NEW.csv','wb')
-fieldnames = ["geonames_id","city_name", "toponym_name", "lat", "lon", "population_rank", "pop", "country_name", "country_code", "un_country_code","region_code", "region_name", "continent_code", "continent_name","citation"]
+fieldnames = ["geonames_id","city_name", "toponym_name", "lat", "lon", "pop", "country_name", "country_code", "un_country_code","region_code", "region_name", "continent_code", "continent_name", "capital", "global_north","global_south","east","west"]
 csvwriter = csv.DictWriter(test_file, delimiter=',', fieldnames=fieldnames)
 csvwriter.writeheader()
 
@@ -29,48 +28,67 @@ class DictUnicodeProxy(object):
 
 for row in CITIES:
 	new_row ={}
-	try:
-		params = {'q': row["city_name"] + "," + row["country_name"], 'maxRows':1, 'lang':'en', 'style':'LONG','featureClass':'P','username':'kanarinka' }
-		
-		r = requests.get(GEONAMES, params=params)
-		#print r.url
-		#print json.dumps(r.json(),sort_keys=True,indent=4, separators=(',', ': '))
-		
-		res = r.json()
-		if res.get("geonames") == None or (res.get("geonames") is not None and len(res["geonames"]) == 0):
-			#print 'No match. Trying with just city name.'
+	new_row["geonames_id"] = row["geonames_id"]
+	new_row["city_name"] = row["city_name"]
+	new_row["toponym_name"] = row["toponym_name"]
+	new_row["lat"] = row["lat"]
+	new_row["lon"] = row["lon"]
+	new_row["pop"] = row["pop"]
+	new_row["country_name"] = row["country_name"]
+	new_row["country_code"] = row["country_code"]
+	new_row["un_country_code"] = row["un_country_code"]
+	new_row["region_code"] = row["region_code"]
+	new_row["region_name"] = row["region_name"]
+	new_row["continent_code"] = row["continent_code"]
+	new_row["continent_name"] = row["continent_name"]
+	new_row["capital"] = row["capital"]
+	new_row["global_south"] = row["global_south"]
+	new_row["global_north"] = row["global_north"]
+	new_row["east"] = row["east"]
+	new_row["west"] = row["west"]
+
+	#LOOKUP FROM GEONAMES.ORG ONLY THE ROWS THAT DO NOT ALREADY HAVE ID INFO
+	if row["geonames_id"] == "" or row["country_code"] == "CN":
+		print row["city_name"] + " has no geonames ID or it's in China so we will look it up"
+		try:
+			params = {'q': row["city_name"] + "," + row["country_name"], 'maxRows':1, 'lang':'en', 'style':'LONG','featureClass':'P','username':'kanarinka' }
 			
-			params = {'q': row["city_name"], 'maxRows':1, 'lang':'en', 'style':'LONG','featureClass':'P','username':'kanarinka' }
 			r = requests.get(GEONAMES, params=params)
+			#print r.url
+			#print json.dumps(r.json(),sort_keys=True,indent=4, separators=(',', ': '))
 			
 			res = r.json()
 			if res.get("geonames") == None or (res.get("geonames") is not None and len(res["geonames"]) == 0):
-				print "NO GEONAMES MATCH FOR " + row["city_name"] + ", " + row["country_name"]
-				new_row["city_name"] = row["city_name"]
-				new_row["country_name"] = row["country_name"]
-				new_row["population_rank"] = row["population_rank"]
-				new_row["lat"] = row["lat"]
-				new_row["lon"] = row["long"]
-				csvwriter.writerow(DictUnicodeProxy(new_row))
-				continue
+				#print 'No match. Trying with just city name.'
+				
+				params = {'q': row["city_name"], 'maxRows':1, 'lang':'en', 'style':'LONG','featureClass':'P','username':'kanarinka' }
+				r = requests.get(GEONAMES, params=params)
+				
+				res = r.json()
+				if res.get("geonames") == None or (res.get("geonames") is not None and len(res["geonames"]) == 0):
+					print "NO GEONAMES MATCH FOR " + row["city_name"] + ", " + row["country_name"]
+					
+					csvwriter.writerow(DictUnicodeProxy(new_row))
+					continue
 
-		resGeo = res["geonames"][0]
+			resGeo = res["geonames"][0]
 
-		new_row["geonames_id"] = resGeo["geonameId"]
-		new_row["city_name"] = resGeo["name"]
-		new_row["toponym_name"] = resGeo["toponymName"]
-		new_row["lat"] = resGeo["lat"]
-		new_row["lon"] = resGeo["lng"]
-		new_row["population_rank"] = row["population_rank"]
-		new_row["pop"] = resGeo["population"]
-		new_row["country_name"] = resGeo["countryName"]
-		new_row["country_code"] = resGeo["countryCode"]
-		
-		
-	except requests.exceptions.RequestException as e:
-		print "ERROR RequestException " + str(e)
+			new_row["geonames_id"] = resGeo["geonameId"]
+			new_row["city_name"] = resGeo["name"]
+			new_row["toponym_name"] = resGeo["toponymName"]
+			new_row["lat"] = resGeo["lat"]
+			new_row["lon"] = resGeo["lng"]
+			new_row["pop"] = resGeo["population"]
+			new_row["country_name"] = resGeo["countryName"]
+			new_row["country_code"] = resGeo["countryCode"]
+			
+			
+		except requests.exceptions.RequestException as e:
+			print "ERROR RequestException " + str(e)
+	
+	csvwriter.writerow(DictUnicodeProxy(new_row))
 
-	found = False
+	'''found = False
 	for row_inner in COUNTRIES:
 		if row_inner["country_code"] == resGeo["countryCode"]:
 			new_row["un_country_code"] = row_inner["un_country_code"]
@@ -85,6 +103,7 @@ for row in CITIES:
 	if not found:
 		print "No country data match for " + row["city_name"] + ", " + row["country_name"]
 		csvwriter.writerow(DictUnicodeProxy(new_row))
+		'''
 
 
 
